fcaa9484bb31a3e83b425e90a670f028d4a99b4d
Dominik Barbist <d.barbist@hotmail.com>
2020-08-15 10:26:41 +0200

    Camera rotation added

--- Assets/Scripts/FlyCamera.cs
+++ Assets/Scripts/FlyCamera.cs
@@ -2,17 +2,18 @@
 
 public class FlyCamera : MonoBehaviour
     {
+    public Transform target;
     float cameraSpeed = 40f;
-    void Update()
-        {
-        Vector3 p = GetBaseInput();
-        p *= Time.deltaTime;
-        Vector3 newPosition = transform.position;
-        transform.Translate(p);
-        newPosition.x = transform.position.x;
-        newPosition.z = transform.position.z;
-        transform.position = newPosition;
-        }
+    public float distance = 2.0f;
+    public float distanceMin = 10f;
+    public float distanceMax = 10f;
+    float rotationYAxis = 0.0f;
+    float rotationXAxis = 0.0f;
+    float velocityX = 0.0f;
+    float velocityY = 0.0f;
+    public float yMinLimit = -90f;
+    public float yMaxLimit = 90f;
+    public float smoothTime = 2f;
 
     private Vector3 GetBaseInput()
         {
@@ -34,5 +35,27 @@ public class FlyCamera : MonoBehaviour
             p_Velocity += new Vector3(cameraSpeed, 0, 0);
             }
         return p_Velocity;
+        }
+    private void LateUpdate()
+        {
+        Vector3 p = GetBaseInput();
+        p *= Time.deltaTime;
+        Vector3 newPosition = transform.position;
+        transform.Translate(p);
+        newPosition.x = transform.position.x;
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
+        if (Input.GetMouseButton(1))
+            {
+            rotationYAxis += cameraSpeed * 2 *  Input.GetAxis("Mouse X") * distance * 0.02f;
+            rotationXAxis -= cameraSpeed * 3 * Input.GetAxis("Mouse Y") * 0.02f;
+            }
+        Quaternion toRotation = Quaternion.Euler(rotationXAxis, rotationYAxis, 0);
+        Quaternion rotation = toRotation;
+
+        transform.position = newPosition;
+        transform.rotation = rotation;
+        velocityX = Mathf.Lerp(velocityX, 0, Time.deltaTime * smoothTime);
+        velocityY = Mathf.Lerp(velocityY, 0, Time.deltaTime * smoothTime);
         }
     }
