c86e50308f78f6cb48a122dc8b0e71f72d9f80d5
Dominik Barbist <d.barbist@hotmail.com>
2020-09-12 17:03:11 +0200

    When there are more than one resouce of the same typ in the resouceCollectingArea, then  the resouces quantities will be combined.

--- Assets/Scripts/Buildings/ResourcBuildingBase.cs
+++ Assets/Scripts/Buildings/ResourcBuildingBase.cs
@@ -1,6 +1,5 @@
 ﻿using Assets.Scripts.Buildings.ResourceBuildings;
 using ResourceGeneration.ResourceVariationen;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -10,19 +9,69 @@ namespace Assets.Scripts.Buildings
     class ResourcBuildingBase : MonoBehaviour
         {
         public int GatheredResourcesOverall;
+        public List<ResouceQuantityTyps> GatherableResouces;
         public int StortedResources;
         public float ProduktionSpeed;
         public int WorkingPeople;
-        public float ResourceCollectingRadius = 20;
+        private float ResourceCollectingRadius = 40;
         public Vector3 BuildingPosition;
         private List<string> ListOfGatherableResources;
         public List<GameObject> GatherableResouceInArea;
 
-        private void Start()
+        public class ResouceQuantityTyps
+            {
+            public int resouceQuantity;
+            public string resouceName;
+            }
+        public void Start()
             {
             BuildingPosition = GetComponent<Transform>().position;
             ListOfGatherableResources = GetComponent<IResourcBuilding>().ResouceToGather;
             GatherableResouceInArea = GetUsableResources(ListOfGatherableResources);
+            GatherableResouces = GetResouceQuantityInArea(GatherableResouceInArea);
+            }
+
+        private List<ResouceQuantityTyps> GetResouceQuantityInArea(List<GameObject> gatherableResouceInArea)
+            {
+            List<ResouceQuantityTyps> resouceQuantityInArea = new List<ResouceQuantityTyps>();
+            foreach (var resoucePatch in gatherableResouceInArea)
+                {
+                var resouceScript = resoucePatch.GetComponent<ResourceBase>();
+                ResouceQuantityTyps resouceQuantityTyps = new ResouceQuantityTyps
+                    {
+                    resouceName = resouceScript.ResourceName
+                    };
+                if (resouceQuantityInArea.Count == 0)
+                    {
+                    resouceQuantityTyps.resouceQuantity = resouceScript.quantityOfTheResource;
+                    resouceQuantityInArea.Add(resouceQuantityTyps);
+                    }
+                else
+                    {
+                    int x = 0;
+                    foreach (var resouceInList in resouceQuantityInArea)
+                        {
+                        if (resouceInList.resouceName.Contains(resouceQuantityTyps.resouceName))
+                            {
+                            resouceInList.resouceQuantity += resouceScript.quantityOfTheResource;
+                            break;
+                            }
+                        else if (!resouceInList.resouceName.Contains(resouceQuantityTyps.resouceName) && !(x < resouceQuantityInArea.Count))
+                            {
+                            x++;
+                            continue;
+                            }
+                        else
+                            {
+                            resouceQuantityTyps.resouceQuantity = resouceScript.quantityOfTheResource;
+                            resouceQuantityInArea.Add(resouceQuantityTyps);
+                            break;
+                            }
+                        }
+                    }
+                }
+
+            return resouceQuantityInArea;
             }
 
         private GameObject[] ScannForResources(float radius, Vector3 startpoint, int maxScannedResources)
