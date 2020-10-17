e2c9f065a16b3e609f6bd09cb9955ebd868cf7e6
Dominik Barbist <d.barbist@hotmail.com>
2020-10-17 08:17:36 +0200

    When adding a building to an existing building there will be a check if a gameobject is in the way.

--- Assets/Scripts/Buildings/BuildingSystemHelper/GetChildBuildingPosition.cs
+++ Assets/Scripts/Buildings/BuildingSystemHelper/GetChildBuildingPosition.cs
@@ -1,58 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Buildings.BuildingSystemHelper
     {
     public class GetChildBuildingPosition
         {
-        public static  Vector3 GetPosition(int couplingPosition, GameObject selectedGameobject)
+        public static Vector3 GetPosition(int couplingPosition, GameObject selectedGameobject)
             {
             //North
             if (couplingPosition == 1)
                 {
                 Vector3 newPosition = new Vector3(selectedGameobject.transform.position.x, selectedGameobject.transform.position.y, selectedGameobject.transform.position.z + selectedGameobject.transform.lossyScale.z);
-                if (HaveNeightbourAtPosition(newPosition, selectedGameobject.transform.parent.GetComponent<IBuildingManagment>().ListOfChildren))
-                    {
-                    return selectedGameobject.transform.position;
-                    }
-                return newPosition;
+                if (IsPositionFree(newPosition, selectedGameobject))
+                    return ReturnLogic(selectedGameobject, newPosition);
+                return selectedGameobject.transform.position;
                 }
             //East
             else if (couplingPosition == 2)
                 {
                 Vector3 newPosition = new Vector3(selectedGameobject.transform.position.x + selectedGameobject.transform.lossyScale.x, selectedGameobject.transform.position.y, selectedGameobject.transform.position.z);
-                if (HaveNeightbourAtPosition(newPosition, selectedGameobject.transform.parent.GetComponent<IBuildingManagment>().ListOfChildren))
-                    {
-                    return selectedGameobject.transform.position;
-                    }
-                return newPosition;
+                if (IsPositionFree(newPosition, selectedGameobject))
+                    return ReturnLogic(selectedGameobject, newPosition);
+                return selectedGameobject.transform.position;
                 }
             //South
             else if (couplingPosition == 3)
                 {
                 Vector3 newPosition = new Vector3(selectedGameobject.transform.position.x, selectedGameobject.transform.position.y, selectedGameobject.transform.position.z - selectedGameobject.transform.lossyScale.z);
-                if (HaveNeightbourAtPosition(newPosition, selectedGameobject.transform.parent.GetComponent<IBuildingManagment>().ListOfChildren))
-                    {
-                    return selectedGameobject.transform.position;
-                    }
-                return newPosition;
+                if (IsPositionFree(newPosition, selectedGameobject))
+                    return ReturnLogic(selectedGameobject, newPosition);
+                return selectedGameobject.transform.position;
                 }
             //West
             else if (couplingPosition == 4)
                 {
                 Vector3 newPosition = new Vector3(selectedGameobject.transform.position.x - selectedGameobject.transform.lossyScale.x, selectedGameobject.transform.position.y, selectedGameobject.transform.position.z);
-                if (HaveNeightbourAtPosition(newPosition, selectedGameobject.transform.parent.GetComponent<IBuildingManagment>().ListOfChildren))
-                    {
-                    return selectedGameobject.transform.position;
-                    }
-                return newPosition;
+                if (IsPositionFree(newPosition, selectedGameobject))
+                    return ReturnLogic(selectedGameobject, newPosition);
+                return selectedGameobject.transform.position;
                 }
             return selectedGameobject.transform.position;
             }
+
+        private static Vector3 ReturnLogic(GameObject selectedGameobject, Vector3 newPosition)
+            {
+            if (HaveNeightbourAtPosition(newPosition, selectedGameobject.transform.parent.GetComponent<IBuildingManagment>().ListOfChildren))
+                {
+                return selectedGameobject.transform.position;
+                }
+            return newPosition;
+            }
+
         private static bool HaveNeightbourAtPosition(Vector3 newPosition, List<GameObject> neighbourPosititions)
             {
             foreach (var neighbour in neighbourPosititions)
@@ -64,6 +62,26 @@ namespace Assets.Scripts.Buildings.BuildingSystemHelper
                 }
             return false;
             }
-
+        private static bool IsPositionFree(Vector3 position, GameObject selectedGameobject)
+            {
+            Collider[] collidersInArea = new Collider[20];
+            _ = Physics.OverlapSphereNonAlloc(position, 1, collidersInArea);
+            foreach (Collider collider in collidersInArea)
+                {
+                if (collider == null)
+                    {
+                    break;
+                    }
+                if (collider.transform.parent == selectedGameobject.transform.parent)
+                    {
+                    continue;
+                    }
+                else if (collider.gameObject.layer == 8 || collider.gameObject.layer == 9 || collider.gameObject.layer == 10)
+                    {
+                    return false;
+                    }
+                }
+            return true;
+            }
         }
     }
